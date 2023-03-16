34787520fadf6e452b92a145050038ae9a13f552
Ben Sikora <70484672+ben-sikora@users.noreply.github.com>
2023-03-15 18:04:12 -0700

    Styling Changes to background and font

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import Scroll from './Scroll'; 
+import './App.css'; 
 
 class App extends React.Component{
     constructor(){
@@ -32,7 +33,7 @@ class App extends React.Component{
         } else{
             return(
                 <div className='tc'>
-                    <h1>RoboFriends </h1>
+                    <h1 className= 'f1'>RoboFriends </h1>
                     <SearchBox searchChange={this.onSearchChange}/>
                     <Scroll>
                         <CardList robots={filteredRobots}/>
